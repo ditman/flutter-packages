Fluttgith1/packages

Rename MarkerType enum values

packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapMarkerController.m
@@ -551,7 +551,7 @@
   CLLocationCoordinate2D position = [FLTMarkersController getPosition:markerToAdd];
   NSString *markerIdentifier = markerToAdd[@"markerId"];
   NSString *clusterManagerIdentifier = markerToAdd[@"clusterManagerId"];
-  GMSMarker *marker = (self.markerType == FGMPlatformMarkerTypeAdvanced)
+  GMSMarker *marker = (self.markerType == FGMPlatformMarkerTypeAdvancedMarker)
                           ? [GMSAdvancedMarker markerWithPosition:position]
                           : [GMSMarker markerWithPosition:position];
   FLTGoogleMapMarkerController *controller =

ditman/flutter-packages

packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapMarkerController.m
@@ -476,7 +476,7 @@
   CLLocationCoordinate2D position = FGMGetCoordinateForPigeonLatLng(markerToAdd.position);
   NSString *markerIdentifier = markerToAdd.markerId;
   NSString *clusterManagerIdentifier = markerToAdd.clusterManagerId;
-    GMSMarker *marker = (self.markerType == FGMPlatformMarkerTypeAdvanced)
+  GMSMarker *marker = (self.markerType == FGMPlatformMarkerTypeAdvanced)
                           ? [GMSAdvancedMarker markerWithPosition:position]
                           : [GMSMarker markerWithPosition:position];
 